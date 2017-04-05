c803cc6d888ca74aa06f42315431730c5795978b
Vivian Tsai <vtsai42@gmail.com>
2017-04-05 15:32:36 -0400

    added initial date/time to FindRideActivity

--- app/src/main/java/io/github/viv2d2/findride/FindRideActivity.java
+++ app/src/main/java/io/github/viv2d2/findride/FindRideActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class FindRideActivity extends AppCompatActivity {
 
     private String from;
@@ -14,6 +17,10 @@ public class FindRideActivity extends AppCompatActivity {
     private String time;
     private TextView input_from;
     private TextView input_to;
+    private TextView input_date;
+    private TextView input_time;
+    private SimpleDateFormat sdf_date;
+    private SimpleDateFormat sdf_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +28,27 @@ public class FindRideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_find_ride);
 
         // Initialize variables
+        input_from = (TextView) findViewById(R.id.from_input);
+        input_to = (TextView) findViewById(R.id.to_input);
+        input_date = (TextView) findViewById(R.id.date_input);
+        input_time = (TextView) findViewById(R.id.time_input);
+        // Date, time formats
+        sdf_date = new SimpleDateFormat("EEE MMM dd yyyy");
+        sdf_time = new SimpleDateFormat("hh:mm a");
+
+
+        // Set initial to/from
         from = "Homewood";
         to = "BWI";
-        input_from = (TextView) findViewById(R.id.from_input);
         input_from.setText(from);
-        input_to = (TextView) findViewById(R.id.to_input);
         input_to.setText(to);
 
+        // Set initial date/time
+        Calendar cal = Calendar.getInstance();
+        time = sdf_time.format(cal.getTime());
+        input_time.setText(time);
+        date = sdf_date.format(cal.getTime());
+        input_date.setText(date);
     }
 
     public void from(View view) {
@@ -50,6 +71,14 @@ public class FindRideActivity extends AppCompatActivity {
         intent.putExtra("location", to);
         // SelectLocActivity == 1
         startActivityForResult(intent, 1);
+    }
+
+    public void date(View view) {
+        // create new intent
+    }
+
+    public void time(View view) {
+        // create new intent
     }
 
     @Override
