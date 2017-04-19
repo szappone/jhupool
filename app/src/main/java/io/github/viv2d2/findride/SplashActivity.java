95303408ff5cfab8b5437ab7e07c074a70efceca
Sarah <SarahZappone@Sarahs-MacBook-Pro-2.local>
2017-04-19 15:25:15 -0400

    final touches before presentation

--- app/src/main/java/io/github/viv2d2/findride/SplashActivity.java
+++ app/src/main/java/io/github/viv2d2/findride/SplashActivity.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 
 public class SplashActivity extends Activity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 4000;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
