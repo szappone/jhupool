a006e4e687a6a5094f4b619b4176856f81e2fd4d
Vivian Tsai <vtsai42@gmail.com>
2017-04-21 22:55:11 -0400

    added my rides xml

--- app/src/main/java/io/github/viv2d2/findride/MyRidesFragment.java
+++ app/src/main/java/io/github/viv2d2/findride/MyRidesFragment.java
@@ -13,6 +13,6 @@ public class MyRidesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Declare your first fragment here
-        return inflater.inflate(R.layout.fragment_all_rides, container, false);
+        return inflater.inflate(R.layout.fragment_my_rides, container, false);
     }
 }
