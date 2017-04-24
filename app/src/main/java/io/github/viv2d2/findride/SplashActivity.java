33aba54bf959355d2512d0ced787bf75312dac37
Will Matteson <willmatteson@traplord.local>
2017-04-24 17:40:37 -0400

    Facebook functionality

--- app/src/main/java/io/github/viv2d2/findride/SplashActivity.java
+++ app/src/main/java/io/github/viv2d2/findride/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends Activity {
         new Handler().postDelayed(new Runnable(){
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
