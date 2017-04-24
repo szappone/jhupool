b6a0fc0e25509930c579ab8d892fb15e306e8bac
Sarah <SarahZappone@Sarahs-MacBook-Pro-2.local>
2017-04-24 15:57:03 -0400

    locked orientation in the manifest and made the switch intuitive

--- app/src/main/java/io/github/viv2d2/findride/MainActivity.java
+++ app/src/main/java/io/github/viv2d2/findride/MainActivity.java
@@ -1,24 +1,17 @@
 package io.github.viv2d2.findride;
 
-import android.app.ActionBar;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.PagerAdapter;
-import android.support.v7.widget.Toolbar;
 import android.support.v4.app.FragmentPagerAdapter;
-
-import android.support.v7.app.AppCompatActivity;
-import android.app.FragmentTransaction;
-import android.app.TabActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
-import java.util.*;
+import java.util.List;
+import java.util.Vector;
 
 
 /**
@@ -30,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         setContentView(R.layout.activity_main);
         // Initializing tab and pager views
