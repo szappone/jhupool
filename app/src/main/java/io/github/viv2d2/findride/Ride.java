e2d776624d9a5ec6fbe200e2dba544a3542b207d
Vivian Tsai <vtsai42@gmail.com>
2017-04-25 14:04:36 -0400

    finished and altered Ride object

--- app/src/main/java/io/github/viv2d2/findride/Ride.java
+++ app/src/main/java/io/github/viv2d2/findride/Ride.java
@@ -11,30 +11,96 @@ public class Ride {
     private String to;
     private String date;
     private String time;
-    private int numRiders;
     private ArrayList<String> riders;
-    private ArrayList<String> notes;
+    private ArrayList<String []> notes;
+    private String category;
 
+    /** Ride constructor (empty). */
     public Ride() {
         // empty constructor
     }
 
-    public Ride(String f, String t, String d, String ti, int numR, String r, String n) {
+    /** Ride constructor. */
+    public Ride(String f, String t, String d, String ti, String r, String n) {
         this.from = f;
         this.to = t;
         this.date = d;
         this.time = ti;
-        this.numRiders = numR;
         this.riders = new ArrayList<String>();
         this.riders.add(r);
-        this.notes = new ArrayList<String>();
-        this.notes.add(n);
+        this.notes = new ArrayList<String []>();
+        String s [] = {r, n};
+        this.notes.add(s);
+
+        // set category
+        if (this.from.equals("BWI") || this.to.equals("BWI")) {
+            this.category = "travel";
+        } else if (this.from.equals("Hampdenfest") || this.to.equals("Hampdenfest")) {
+            this.category = "special";
+        } else {
+            this.category = "groceries";
+        }
     }
 
+    /** Getters. */
     public String getFrom() { return this.from; }
     public String getTo() { return this.to; }
     public String getDate() { return this.date; }
     public String getTime() { return this.time; }
-    public int getNumRiders() { return this.numRiders; }
+    public int getNumRiders() { return this.riders.size(); }
+    public ArrayList<String> getRiders() { return this.riders; }
+    public ArrayList<String []> getNotes() { return this.notes; }
+
+    /** Returns note corresponding to given rider r. */
+    public String getNoteFromRider(String r) {
+        for (int i = 0; i < this.notes.size(); ++i) {
+            if (this.notes.get(i)[0].equals(r)) {
+                return this.notes.get(i)[1];
+            }
+        }
+        return "";
+    }
+
+    /** Sets note n for a specific rider r. */
+    public boolean setNoteToRider(String r, String n) {
+        for (int i = 0; i < this.notes.size(); ++i) {
+            if (this.notes.get(i)[0].equals(r)) {
+                this.notes.get(i)[1] = n;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Adds note n to the notes of rider r. */
+    public boolean addNoteToRider(String r, String n) {
+        for (int i = 0; i < this.notes.size(); ++i) {
+            if (this.notes.get(i)[0].equals(r)) {
+                this.notes.get(i)[1] += "\n" + n;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Adds rider to rider array, notes array. */
+    public void addRider(String r, String n) {
+        this.riders.add(r);
+        String [] s = {r, n};
+        this.notes.add(s);
+    }
+
+    /** Deletes rider from rider array, notes array. */
+    public boolean deleteRider(String r) {
+        for (int i = 0; i < this.riders.size(); ++i) {
+            if (this.riders.get(i).equals(r)) {
+                this.riders.remove(i);
+                this.notes.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
