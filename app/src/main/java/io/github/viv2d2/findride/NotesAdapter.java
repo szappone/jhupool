1f43e17c3f1a5cfd7da5b82637d15824a72395ce
Vivian Tsai <vtsai42@gmail.com>
2017-04-27 21:31:06 -0400

    created NotesAdapter

--- app/src/main/java/io/github/viv2d2/findride/NotesAdapter.java
+++ app/src/main/java/io/github/viv2d2/findride/NotesAdapter.java
@@ -1,8 +1,53 @@
 package io.github.viv2d2.findride;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
- * Created by Vivian on 4/27/17.
+ * ArrayAdapter for rides.
  */
 
-public class NotesAdapter {
+public class NotesAdapter extends ArrayAdapter<Rider> {
+
+    int resource;
+
+    public NotesAdapter(Context ctx, int r, List<Rider> riders)  {
+        super(ctx, r, riders);
+        this.resource = r;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LinearLayout notesView;
+        Rider rider = getItem(position);
+
+        if (convertView == null) {
+            notesView = new LinearLayout(getContext());
+            String inflater = Context.LAYOUT_INFLATER_SERVICE;
+            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(inflater);
+            vi.inflate(this.resource, notesView, true);
+        } else {
+            notesView = (LinearLayout) convertView;
+        }
+
+        ImageView input_profile = (ImageView) notesView.findViewById(R.id.profile);
+        TextView input_notes = (TextView) notesView.findViewById(R.id.notes);
+
+        // Set up notes
+        String notes_input = rider.getNotes();
+        input_notes.setText(notes_input);
+
+
+        // Set up icon
+
+        return notesView;
+    }
 }
