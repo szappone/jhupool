9b4559eefb4a113e4ec3ffc9ff9754da6b006707
Vivian Tsai <vtsai42@gmail.com>
2017-04-28 04:31:36 -0400

    populate RideResults with rides that user is not part of

--- app/src/main/java/io/github/viv2d2/findride/FindRideResultsActivity.java
+++ app/src/main/java/io/github/viv2d2/findride/FindRideResultsActivity.java
@@ -1,10 +1,22 @@
 package io.github.viv2d2.findride;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * Shows results (from matching algorithm) and user's car (from search).
@@ -23,10 +35,15 @@ public class FindRideResultsActivity extends AppCompatActivity {
     private TextView preview_dateTime;
     private TextView preview_riders;
 
+    protected static ArrayList<Ride> rideResults;
+    protected static RideAdapter resultAdapter;
+    private ListView resultsView;
+    private View rootView;
+    private String jhed;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_ride_results);
-
 
         // User's car (from search)
         user_from = getIntent().getStringExtra("from");
@@ -36,14 +53,80 @@ public class FindRideResultsActivity extends AppCompatActivity {
         user_riders = getIntent().getStringExtra("riders");
         user_notes = getIntent().getStringExtra("notes");
 
-        // Set up preview
+        // Set up preview for search
         preview_fromTo = (TextView) findViewById(R.id.create_fromTo);
         preview_dateTime = (TextView) findViewById(R.id.create_dateTime);
         preview_riders = (TextView) findViewById(R.id.create_riders);
 
         preview_fromTo.setText(user_from + " -> " + user_to);
         preview_dateTime.setText(user_date + ", " + user_time);
-        preview_riders.setText(user_riders + " riders");
+        if (Integer.parseInt(user_riders) == 1) {
+            preview_riders.setText(user_riders + " rider");
+        } else {
+            preview_riders.setText(user_riders + " riders");
+        }
+
+
+        // Populate results
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        jhed = settings.getString("JHED_ID", "");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myDB = database.getReference();
+
+        rideResults = new ArrayList<Ride>();
+
+        myDB.child("Drive_Feed").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    Ride ride = child.getValue(Ride.class);
+                    if (!ride.inCar(jhed)) {
+                        rideResults.add(ride);
+                    }
+                }
+
+                resultAdapter = new RideAdapter(FindRideResultsActivity.this, R.layout.ride_preview, rideResults);
+                resultsView.setAdapter(resultAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError firebaseError) {
+
+            }
+        });
+
+        resultsView = (ListView) findViewById(R.id.results);
+
+        // Set up adapter
+        resultAdapter = new RideAdapter(this, R.layout.ride_preview, rideResults);
+        resultsView.setAdapter(resultAdapter);
+
+        // View a result
+
+        resultsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object temp = resultsView.getItemAtPosition(position);
+                Ride currRide = (Ride) temp;
+
+                Intent intent = new Intent(FindRideResultsActivity.this, ViewRideActivity.class);
+                intent.putExtra("action", 1); // join car
+                intent.putExtra("from", currRide.getFrom());
+                intent.putExtra("to", currRide.getTo());
+                intent.putExtra("date", currRide.getDate());
+                intent.putExtra("time", currRide.getTime());
+                intent.putExtra("riders", Integer.toString(currRide.getNumRiders()));
+                intent.putExtra("riderObjects", currRide.getRiders());
+                intent.putExtra("r", currRide);
+
+                startActivity(intent);
+
+            }
+        });
     }
 
     public void viewRide(View view) {
