d196c14c5c38905956f47304d88bf3713b3430bd
Sarah <SarahZappone@Sarahs-MacBook-Pro-2.local>
2017-04-28 15:38:43 -0400

    cleaed FindRideResultsAcitivty

--- app/src/main/java/io/github/viv2d2/findride/FindRideResultsActivity.java
+++ app/src/main/java/io/github/viv2d2/findride/FindRideResultsActivity.java
@@ -34,16 +34,11 @@ public class FindRideResultsActivity extends AppCompatActivity {
     private String user_riders;
     private String user_notes;
 
-    private TextView preview_fromTo;
-    private TextView preview_dateTime;
-    private TextView preview_riders;
-
     protected static ArrayList<Ride> rideResults;
     protected static RideAdapter resultAdapter;
-    private ListView resultsView;
-    private View rootView;
-    private String jhed;
     static int THREE_HOURS = 10800000;
+    private ListView resultsView;
+    private String jhed;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,9 +53,9 @@ public class FindRideResultsActivity extends AppCompatActivity {
         user_notes = getIntent().getStringExtra("notes");
 
         // Set up preview for search
-        preview_fromTo = (TextView) findViewById(R.id.create_fromTo);
-        preview_dateTime = (TextView) findViewById(R.id.create_dateTime);
-        preview_riders = (TextView) findViewById(R.id.create_riders);
+        TextView preview_fromTo = (TextView) findViewById(R.id.create_fromTo);
+        TextView preview_dateTime = (TextView) findViewById(R.id.create_dateTime);
+        TextView preview_riders = (TextView) findViewById(R.id.create_riders);
         preview_fromTo.setText(user_from + " -> " + user_to);
         preview_dateTime.setText(user_date + ", " + user_time);
         if (Integer.parseInt(user_riders) == 1) {
@@ -69,7 +64,6 @@ public class FindRideResultsActivity extends AppCompatActivity {
             preview_riders.setText(user_riders + " riders");
         }
 
-
         // Populate results
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
         jhed = settings.getString("JHED_ID", "");
@@ -77,7 +71,6 @@ public class FindRideResultsActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myDB = database.getReference();
         rideResults = new ArrayList<Ride>();
-
 
          myDB.child("Drive_Feed").addValueEventListener(new ValueEventListener() {
             @Override
@@ -88,10 +81,8 @@ public class FindRideResultsActivity extends AppCompatActivity {
                     Ride ride = child.getValue(Ride.class);
                     rideResults = runMatchingAlgorithm(ride);
                 }
-
                 resultAdapter = new RideAdapter(FindRideResultsActivity.this, R.layout.ride_preview, rideResults);
                 resultsView.setAdapter(resultAdapter);
-
             }
 
             @Override
@@ -107,7 +98,6 @@ public class FindRideResultsActivity extends AppCompatActivity {
         resultsView.setAdapter(resultAdapter);
 
         // View a result
-
         resultsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -125,7 +115,6 @@ public class FindRideResultsActivity extends AppCompatActivity {
                 intent.putExtra("r", currRide);
 
                 startActivity(intent);
-
             }
         });
     }
@@ -150,7 +139,6 @@ public class FindRideResultsActivity extends AppCompatActivity {
             date2 = parseFormat.parse(possible);
         } catch (ParseException p) {
         }
-        System.out.println("Elapsed D1 - D2: " + Math.abs(date1.getTime() - date2.getTime()));
         return Math.abs(date1.getTime() - date2.getTime()) <= THREE_HOURS;
     }
 
